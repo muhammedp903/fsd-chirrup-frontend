334ab33ca39351dcc63e64fb7057d1d61dfd5eb6
Muhammed Patel <muhammedp903@gmail.com>
2023-11-14 17:13:57 +0000

    Login page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
-    <h1>Hello, World!</h1>
+    <h1>{{ welcome_message }}</h1>
+    <p>Counter: {{counter}}</p>
+    <button v-on:click="increment">Add One</button>
+    <p v-if="counter >= 5">Big number</p>
+    <p v-else>Little number</p>
   </div>
 </template>
+
+<script>
+  export default {
+    data(){
+      return {
+        welcome_message: "Hello, World!",
+        counter: 0
+      }
+    },
+    methods: {
+      increment(){
+        this.counter ++;
+      }
+    },
+    components: {
+      Login
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <form>
+            <label for="email">Email: </label>
+            <input type="email" name="email"/>
+
+            <br/><br/>
+
+            <label for="password">Password: </label>
+            <input type="password" name="password"/>
+
+            <button>Login</button>
+        </form>
+    </div>
+</template>
